f8b6a4c7bcba352ec5954a1925f1d37ad5aa3015
emegriffioen <emegriffioen@gmail.com>
2025-10-28 17:12:55 -0600

    added failing test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -4,4 +4,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
 
+    @Test
+    public void testHasCity(){
+        City testCity = new City("Van","BC");
+        CustomList testCities = new CustomList();
+        testCities.addCity(testCity);
+
+        assertTrue(testCities.hasCity(testCity));
+    }
+
 }
